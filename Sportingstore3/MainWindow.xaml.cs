1171e559b4b6aab01a8a555fab33dbc12e119b75
Aleksandr Bynkov <bixber.commercial@yandex.ru>
2023-10-25 21:41:01 +0300

    Add a registration interface for the user

--- Sportingstore3/MainWindow.xaml.cs
+++ Sportingstore3/MainWindow.xaml.cs
@@ -15,32 +15,43 @@ namespace Sportingstore
             InitializeComponent();
         }
 
-    public void enterButton_Click(object sender, RoutedEventArgs e)
-    {
-        string login = loginField.Text;
-        string passphrase = passwordField.Password;
-
-        using (var dbContext = new MyDbContext())
+        public void enterButton_Click(object sender, RoutedEventArgs e)
         {
-            var user = dbContext.Users.FirstOrDefault(
-                u =>
-                u.Login == login &&
-                u.Passphrase == passphrase);
-                
-            if (user != null)
-            {
-                    showcase mainWindow = new showcase();
-                    mainWindow.Show();
+            string login = loginField.Text;
+            string passphrase = passwordField.Password;
 
-                    // Закрытие текущего окна
-                    Window currentWindow = Application.Current.MainWindow;
-                    currentWindow.Close();
-                }
-            else
+            using (var dbContext = new MyDbContext())
             {
-                MessageBox.Show("Неверное имя пользователя или пароль");
+                var user = dbContext.Users.FirstOrDefault(
+                    u =>
+                    u.Login == login &&
+                    u.Passphrase == passphrase);
+                
+                if (user != null)
+                {
+                        showcase mainWindow = new showcase();
+                        mainWindow.Show();
+
+                        // Закрытие текущего окна
+                        Window currentWindow = Application.Current.MainWindow;
+                        currentWindow.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неверное имя пользователя или пароль");
+                }
             }
         }
+
+        public void registerButton_Click(object sender, RoutedEventArgs e) 
+        {
+            RegistrationPage regPage = new RegistrationPage();
+            Window mainWindow = Application.Current.MainWindow;
+            mainWindow.Close();
+            regPage.Show();
+        }
+         
+        }
+
     }
-    }
-}
+
